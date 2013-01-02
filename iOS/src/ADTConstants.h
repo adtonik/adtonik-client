4ace79437ab4893e1131d38d288c8dc98c93be9a
Marshall Allen Beddoe <unmarshal@gmail.com>
2013-01-02 13:40:35 -0800

    include version

--- iOS/src/ADTConstants.h
+++ iOS/src/ADTConstants.h
@@ -13,7 +13,7 @@
 #define ADT_SAMPLE_SECONDS 6
 #define ADT_DEFAULT_REFRESH_TIMER 10
 
-static NSString *ADT_SDK_VERSION = @"1.0.0";
+static NSString *ADT_SDK_VERSION = ADT_BUILD_TAG;
 
 #if ADT_USE_DEV_API == 1
   static NSString *ADT_HOSTNAME = @"http://dev.api.adtonik.net:3000";
