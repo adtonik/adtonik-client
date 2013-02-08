77ec66e80694d6505811d3455071b6918359445e
Marshall Allen Beddoe <unmarshal@gmail.com>
2013-02-07 16:56:39 -0800

    unused

--- iOS/src/ADTConstants.h
+++ iOS/src/ADTConstants.h
@@ -13,12 +13,12 @@
 #define ADT_SAMPLE_SECONDS 6
 #define ADT_DEFAULT_REFRESH_TIMER 10
 
-static NSString *kADTSDKVersion = ADT_BUILD_TAG;
+static NSString *kADTSDKVersion __unused = ADT_BUILD_TAG;
 
 #if ADT_USE_DEV_API == 1
-  static NSString *kADTHostname = @"http://dev.api.adtonik.net:3000";
+  static NSString *kADTHostname __unused = @"http://dev.api.adtonik.net:3000";
 #else
-  static NSString *kADTHostname = @"http://api.adtonik.net";
+  static NSString *kADTHostname __unused = @"http://api.adtonik.net";
 #endif
 
 // Error domain
