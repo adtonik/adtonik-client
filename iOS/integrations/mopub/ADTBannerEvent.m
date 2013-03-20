8e4eb63c08b44bc2412543ea85ab5da3bd72d3fb
Marshall Allen Beddoe <unmarshal@gmail.com>
2013-03-19 18:40:29 -0700

    added ifdef to error unless adtonik app id is defined

--- iOS/integrations/mopub/ADTBannerEvent.m
+++ iOS/integrations/mopub/ADTBannerEvent.m
@@ -9,7 +9,9 @@
 #import "ADTBannerEvent.h"
 #import "ADTUtils.h"
 
-#define kAdTonikAppID @"ADTDemoApp"
+#ifndef kAdTonikAppID
+#error define kAdTonikAppID with your appid
+#endif
 
 @interface ADTBannerEvent () <ADTAdViewDelegate>
 
@@ -24,18 +26,18 @@
 - (void) requestAdWithSize:(CGSize)size customEventInfo:(NSDictionary *)info
 {
   NSLog(@"Requesting AdTonik banner.");
-  
+
   UIViewController *rootViewController = [self.delegate viewControllerForPresentingModalView];
-  
+
   self.adtonikAdView = [[ADTAdView alloc] initWithFrame:CGRectMake(0, 0, size.width, size.height)
                                                   appID:kAdTonikAppID
                                                  adSize:size
                                      rootViewController:rootViewController];
-  
+
   self.adtonikAdView.delegate = self;
-  
+
   ADTAdRequest *request = [ADTAdRequest request];
-  
+
   [self.adtonikAdView loadAdRequest:request];
 }
 
@@ -45,35 +47,35 @@
 - (void)adViewDidFinishRequest:(ADTAdView *)adView
 {
   NSLog(@"Successfully loaded adtonik banner.");
-  
+
   [self.delegate bannerCustomEvent:self didLoadAd:adView];
 }
 
 - (void)adView:(ADTAdView *)adView didFailRequestWithError:(NSError *)error
 {
   NSLog(@"Failed to load adtonik banner.");
-  
+
   [self.delegate bannerCustomEvent:self didFailToLoadAdWithError:error];
 }
 
 - (void)adViewWillPresentScreen:(ADTAdView *)adView
 {
   NSLog(@"AdTonik banner will present screen.");
-  
+
   [self.delegate bannerCustomEventWillBeginAction:self];
 }
 
 - (void)adViewWillDismissScreen:(ADTAdView *)adView
 {
   NSLog(@"AdTonik banner will dismiss screen.");
-  
+
   [self.delegate bannerCustomEventDidFinishAction:self];
 }
 
 - (void)adViewWillLeaveApplication:(ADTAdView *)adView
 {
   NSLog(@"AdTonik banner will leave application.");
-  
+
   [self.delegate bannerCustomEventWillLeaveApplication:self];
 }
 
